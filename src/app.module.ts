363d96769345676245fb6084644fb1a0fcb91392
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2024-06-13 18:45:28 +0300

    full qraphql auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { QraphqlModule } from "./graphql/qraphql.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UsersModule } from "./modules/users/user.module";
 import { DatabaseModule } from "./database/database.module";
 import { Module } from "@nestjs/common";
 import { UsersResolver } from "./modules/users/users.resolver";
+
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule],
+  imports: [QraphqlModule, AuthModule, UsersModule, DatabaseModule],
   controllers: [],
   providers: [UsersResolver],
 })
