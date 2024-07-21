7bcc97d32bf0e757e03befa277461bb50748f699
montleee <144028935+montleee@users.noreply.github.com>
2024-07-21 10:06:08 +0300

    added graphql

--- src/modules/theme/theme.module.ts
+++ src/modules/theme/theme.module.ts
@@ -4,9 +4,10 @@ import { ThemeController } from "./theme.controller";
 import { ThemeService } from "./theme.service";
 import { Theme } from "./model/theme.model";
 import { ThemeResolver } from "./graphQL/theme.resolver";
+import { GraphqlModule } from "../../graphQL/Graphql.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Theme])],
+  imports: [TypeOrmModule.forFeature([Theme]), GraphqlModule],
   providers: [ThemeService, ThemeResolver],
   controllers: [ThemeController],
   exports: [TypeOrmModule.forFeature([Theme])],
